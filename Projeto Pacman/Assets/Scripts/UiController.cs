6c3a59683a90e0980c03dee484f6cf81f2d293e4
Teus42 <teus96@gmail.com>
2020-06-04 21:32:42 -0300

    add Menu
    Adicionado a tela inicial com um menu funcional, além de algumas mudanças no jogo

---
+++ Projeto Pacman/Assets/Scripts/UiController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiController : MonoBehaviour
+{
+    public void Iniciar()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Exit()
+    {
+        Application.Quit();                
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
