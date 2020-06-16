a010328b178c06f16df6386613b323a9eb2b359d
Teus42 <teus96@gmail.com>
2020-06-15 23:09:37 -0300

    Finalizado

--- Projeto Pacman/Assets/Scripts/UiController.cs
+++ Projeto Pacman/Assets/Scripts/UiController.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UiController : MonoBehaviour
 {
+    public GameObject sobre;
+    public Text highscore;
     public void Iniciar()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    void Update()
+    {
+        highscore.text = PlayerPrefs.GetInt("highscore").ToString();
+        Debug.Log(PlayerPrefs.GetInt("highscore"));
     }
     public void Exit()
     {
@@ -13,5 +22,19 @@ public class UiController : MonoBehaviour
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #endif
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.SetInt("highscore",0);
+    }
+
+    public void Sobre()
+    {
+        sobre.SetActive(true);
+    }
+    public void CloseSobre()
+    {
+        sobre.SetActive(false);
     }
 }
